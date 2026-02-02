4d84e9095fe1394f9298ce7c3263e13a0f0c240b
Jkorf <jankorf91@gmail.com>
2026-02-02 09:36:24 +0100

    Added check for disposed client in UserClientProvider

--- BitMEX.Net/Clients/BitMEXUserClientProvider.cs
+++ BitMEX.Net/Clients/BitMEXUserClientProvider.cs
@@ -64,7 +64,7 @@ namespace BitMEX.Net.Clients
         /// <inheritdoc />
         public IBitMEXRestClient GetRestClient(string userIdentifier, ApiCredentials? credentials = null, BitMEXEnvironment? environment = null)
         {
-            if (!_restClients.TryGetValue(userIdentifier, out var client))
+            if (!_restClients.TryGetValue(userIdentifier, out var client) || client.Disposed)
                 client = CreateRestClient(userIdentifier, credentials, environment);
 
             return client;
@@ -73,7 +73,7 @@ namespace BitMEX.Net.Clients
         /// <inheritdoc />
         public IBitMEXSocketClient GetSocketClient(string userIdentifier, ApiCredentials? credentials = null, BitMEXEnvironment? environment = null)
         {
-            if (!_socketClients.TryGetValue(userIdentifier, out var client))
+            if (!_socketClients.TryGetValue(userIdentifier, out var client) || client.Disposed)
                 client = CreateSocketClient(userIdentifier, credentials, environment);
 
             return client;
