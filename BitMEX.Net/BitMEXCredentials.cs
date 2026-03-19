6253754fc332d2c4c2e8e6397aa32d676f1188e9
Jkorf <jankorf91@gmail.com>
2026-03-19 16:35:16 +0100

    wip

--- BitMEX.Net/BitMEXCredentials.cs
+++ BitMEX.Net/BitMEXCredentials.cs
@@ -1,4 +1,5 @@
 ﻿using CryptoExchange.Net.Authentication;
+using System;
 
 namespace BitMEX.Net
 {
@@ -8,12 +9,39 @@ namespace BitMEX.Net
     public class BitMEXCredentials : HMACCredential
     {
         /// <summary>
-        /// Create new credentials providing only credentials in HMAC format
+        /// Create new credentials
+        /// </summary>
+        public BitMEXCredentials() { }
+
+        /// <summary>
+        /// Create new credentials providing credentials in HMAC format
         /// </summary>
         /// <param name="key">API key</param>
         /// <param name="secret">API secret</param>
         public BitMEXCredentials(string key, string secret) : base(key, secret)
         {
+        }
+
+        /// <summary>
+        /// Create new credentials providing HMAC credentials
+        /// </summary>
+        /// <param name="credential">HMAC Credentials</param>
+        public BitMEXCredentials(HMACCredential credential) : base(credential.Key, credential.Secret)
+        {
+        }
+
+        /// <summary>
+        /// Specify the HMAC credentials
+        /// </summary>
+        /// <param name="key">API key</param>
+        /// <param name="secret">API secret</param>
+        public BitMEXCredentials WithHMAC(string key, string secret)
+        {
+            if (!string.IsNullOrEmpty(Key)) throw new InvalidOperationException("Credentials already set");
+
+            Key = key;
+            Secret = secret;
+            return this;
         }
     }
 }
